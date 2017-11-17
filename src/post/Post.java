1d71df8dd676e77f8eb116c0bf522e2b774b9065
Robert Schoffer <robeszpierre@gmail.com>
2017-11-17 11:35:19 +0100

    Menu refreshed

--- src/post/Post.java
+++ src/post/Post.java
@@ -2,10 +2,22 @@ package post;
 
 
 public class Post {
-
-
-    public static void main(String[] args) {
-        Menu.displayMainMenu();
+    private static Boolean finished=false;
+    
+     public static void main(String[] args) {
+        new Post().start();
+        
     }
+
+    private void start() {
+        while(!finished){
+            Menu.displayRegistrationMenu();
+        }
+    }
+
+    public static void finished(){
+        finished=true;
+    }
+   
     
 }
